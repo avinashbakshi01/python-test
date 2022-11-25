f1abeb249572004fbbf4f88c8520850472a3439a
Avinash <39770763+avinashbakshi01@users.noreply.github.com>
2022-11-26 00:10:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import ast
 #uncomment/comment to see the difference in scan results
 #do not enter it will change line number and wont be marked as duplicate
 #AccessKey: "AKIAR1YF3M8L9IBVOUED" 
-eval("__import__('os').system('clear')", {'__builtins__':{}})
-print(1)
+# eval("__import__('os').system('clear')", {'__builtins__':{}})
+# print(1)
 
 SQL_USER=testing
 SQL_PASSWORD=testing
@@ -21,9 +21,9 @@ if __name__ == '__main__':
     cmd_api_client()
 # A user-defined method named "eval" should not get flagged.
 class Test(object):
-    def eval(self):
-        print("hi")
+#     def eval(self):
+#         print("hi")
     def foo(self):
         self.eval()
 
-Test().eval()
+# Test().eval()
