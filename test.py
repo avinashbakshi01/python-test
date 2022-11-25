a3348aecbdc77c4499bd76fe9a9a278e51696fcf
Avinash <39770763+avinashbakshi01@users.noreply.github.com>
2022-11-26 00:26:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import ast
 #uncomment/comment to see the difference in scan results
 #do not enter it will change line number and wont be marked as duplicate
 #AccessKey: "AKIAR1YF3M8L9IBVOUED" 
-# eval("__import__('os').system('clear')", {'__builtins__':{}})
-# print(1)
+eval("__import__('os').system('clear')", {'__builtins__':{}})
+print(1)
 
 SQL_USER=testing
 SQL_PASSWORD=testing
